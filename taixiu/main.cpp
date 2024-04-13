23ff93fb46209d58f399450e75d7c9c11d272c74
dodinhhoanganh <ngoanhmanh82@gmail.com>
2024-04-13 08:12:33 +0700

    ngon

---
+++ taixiu/main.cpp
@@ -0,0 +1,16 @@
+//#include<bits/stdc++.h>
+#include <algorithm>
+#include <math.h>
+#include <climits>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	return 0;
+}
